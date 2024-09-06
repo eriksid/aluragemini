43193f1fba4b4d3001b8a2e5c081bc7362249925
Erik Sidnei Corrêa <erik.sidnei94@gmail.com>
2024-09-05 23:19:25 -0300

    Adicioando filtro

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const search = () => {
   // Seleciona a seção onde os resultados serão exibidos
   let $section = document.querySelector("#resultados-pesquisa");
+  let searchInput = document.querySelector("#search-input").value;
 
   // Inicializa uma string vazia para armazenar os resultados da busca
   let results = '';
 
   // Itera sobre cada atleta na base de dados
   for (athlete of athletes) {
+    if (!athlete.name.toLowerCase().includes(searchInput.toLowerCase())) {
+      continue;
+    }
     // Cria um novo elemento HTML para cada atleta
     results += `
       <div class="item-resultado">
