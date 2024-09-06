8c19111ff3ee6d5bc4d06fb48853914720610c5e
Erik Sidnei Corrêa <erik.sidnei94@gmail.com>
2024-09-05 23:05:40 -0300

    Adicionando base de daos e carregar resultados pelo js

---
+++ dados.js
@@ -0,0 +1,35 @@
+let athletes = [
+  {
+    name: "Rebeca Andrade",
+    modalidade: "Ginasta Artística",
+    conquistas: ["Medalha de Ouro nas Olimpíadas de Tóquio 2020", "Campeã Mundial de Ginasta"],
+    idade: 23,
+    pais: "Brasil",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+  },
+
+  {
+    name: "Rayssa Leal",
+    modalidade: "Skatista",
+    conquistas: ["Medalha de Ouro nas Olimpíadas de Tóquio 2020", "Campeã Mundial de Skatista"],
+    idade: 23,
+    pais: "Brasil",
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+  },
+  {
+    name: "Beatriz Souza",
+    modalidade: "Judô",
+    conquistas: ["Medalha de Ouro nos Jogos Olímpicos de Paris 2024", "Campeã Mundial de Judô"],
+    idade: 25,
+    pais: "Brasil",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+  },
+  {
+    name: "Vitoria Santos",
+    modalidade: "Ginasta Artística",
+    conquistas: ["Medalha de Ouro nas Olimpíadas de Tóquio 2020", "Campeã Mundial de Ginasta"],
+    idade: 23,
+    pais: "Brasil",
+    link: "https://pt.wikipedia.org/wiki/Vitoria_Santos"
+  }
+]
--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("Olá, mundo!");
+let $section = document.querySelector("#resultados-pesquisa");
+//busca na base dados.js
+let results = ''
+
+for (athlete of athletes) {
+  results += `
+    <div class="item-resultado">
+      <h2>${athlete.name}</h2>
+      <p>${athlete.conquistas}</p>
+      <a href="${athlete.link}" target="_blank">Saiba mais</a>
+    </div>`
+}
+
+$section.innerHTML = results
